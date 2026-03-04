df007088ccf2879c136415f3c06ae8072f22bbde
lehie <lehieuhuy19@gmail.com>
2026-03-04 09:41:03 +0700

    Rename to Dancing Mask, add Dancing None Mask node, remove license_key

--- set_value_for_dancing.py
+++ set_value_for_dancing.py
@@ -1,4 +1,4 @@
-class SetValueForDancing:
+class SetValueForDancingMask:
     @classmethod
     def INPUT_TYPES(cls):
         return {
@@ -34,15 +34,10 @@ class SetValueForDancing:
                     "multiline": False,
                     "placeholder": "Đường dẫn thư mục hình ảnh",
                 }),
-                "License_key": ("STRING", {
-                    "default": "",
-                    "multiline": False,
-                    "placeholder": "License_key",
-                }),
             }
         }
 
-    RETURN_TYPES = ("BOOLEAN", "STRING", "BOOLEAN", "STRING", "STRING", "STRING", "STRING", "STRING")
+    RETURN_TYPES = ("BOOLEAN", "STRING", "BOOLEAN", "STRING", "STRING", "STRING", "STRING")
     RETURN_NAMES = (
         "Bật mask",
         "Tách mask",
@@ -51,7 +46,6 @@ class SetValueForDancing:
         "Thư mục lưu video kết quả",
         "Đường dẫn thư mục videos",
         "Đường dẫn thư mục hình ảnh",
-        "License_key",
     )
     FUNCTION = "run"
     CATEGORY = "comfyui-sortlist"
@@ -64,15 +58,63 @@ class SetValueForDancing:
         save_path = kwargs.get("Thư mục lưu video kết quả", "") or ""
         videos_dir = kwargs.get("Đường dẫn thư mục videos", "") or ""
         images_dir = kwargs.get("Đường dẫn thư mục hình ảnh", "") or ""
-        license_key = kwargs.get("License_key", "") or ""
 
-        return (bat_mask, mask_prompt, bat_background, file_name, save_path, videos_dir, images_dir, license_key)
+        return (bat_mask, mask_prompt, bat_background, file_name, save_path, videos_dir, images_dir)
+
+
+class SetValueForDancingNoneMask:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "Tên file cần lưu": ("STRING", {
+                    "default": "",
+                    "multiline": False,
+                    "placeholder": "Tên file cần lưu",
+                }),
+                "Thư mục lưu video kết quả": ("STRING", {
+                    "default": "",
+                    "multiline": False,
+                    "placeholder": "Thư mục lưu video kết quả",
+                }),
+                "Đường dẫn thư mục videos": ("STRING", {
+                    "default": "",
+                    "multiline": False,
+                    "placeholder": "Đường dẫn thư mục videos",
+                }),
+                "Đường dẫn thư mục hình ảnh": ("STRING", {
+                    "default": "",
+                    "multiline": False,
+                    "placeholder": "Đường dẫn thư mục hình ảnh",
+                }),
+            }
+        }
+
+    RETURN_TYPES = ("STRING", "STRING", "STRING", "STRING")
+    RETURN_NAMES = (
+        "Tên file cần lưu",
+        "Thư mục lưu video kết quả",
+        "Đường dẫn thư mục videos",
+        "Đường dẫn thư mục hình ảnh",
+    )
+    FUNCTION = "run"
+    CATEGORY = "comfyui-sortlist"
+
+    def run(self, **kwargs):
+        file_name = kwargs.get("Tên file cần lưu", "") or ""
+        save_path = kwargs.get("Thư mục lưu video kết quả", "") or ""
+        videos_dir = kwargs.get("Đường dẫn thư mục videos", "") or ""
+        images_dir = kwargs.get("Đường dẫn thư mục hình ảnh", "") or ""
+
+        return (file_name, save_path, videos_dir, images_dir)
 
 
 NODE_CLASS_MAPPINGS = {
-    "Set Value For Dancing": SetValueForDancing,
+    "Set Value For Dancing Mask": SetValueForDancingMask,
+    "Set Value For Dancing None Mask": SetValueForDancingNoneMask,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Set Value For Dancing": "Set Value For Dancing",
+    "Set Value For Dancing Mask": "Set Value For Dancing Mask",
+    "Set Value For Dancing None Mask": "Set Value For Dancing None Mask",
 }
